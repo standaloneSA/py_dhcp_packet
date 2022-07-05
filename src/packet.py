5ae17d0f80234d590123c28a3026ab965f8d5c13
Matt Simmons <standalone.sysadmin@gmail.com>
2022-07-05 17:12:58 -0400

    Fixed formatting

--- src/packet.py
+++ src/packet.py
@@ -1,5 +1,6 @@
 """
-The information is taken and interpreted from RFC2131 and RFC2132
+The information is taken and interpreted from the following RFCs:
+https://www.ietf.org/rfc/rfc1533.txt
 https://www.ietf.org/rfc/rfc2131.txt
 https://www.ietf.org/rfc/rfc2132.txt
 
@@ -56,22 +57,22 @@ class packet:
 
     def __init__(
         self,
-        op = typing.Optional[int]=None,
-        htype = typing.Optional[int]=None,
-        hlen = typing.Optional[int]=None,
-        hops = typing.Optional[int]=None,
-        xid = typing.Optional[int]=None,
-        secs = typing.Optional[int]=None,
-        flags = typing.Optional[bytearray]=None,
-        ciaddr = typing.Optional[typing.Union[str, bytearray]]=None,
-        yiaddr = typing.Optional[typing.Union[str, bytearray]]=None,
-        siaddr = typing.Optional[typing.Union[str, bytearray]]=None,
-        giaddr = typing.Optional[typing.Union[str, bytearray]]=None,
-        chaddr = typing.Optional[typing.Union[str, bytearray]]=None,
-        sname = typing.Optional[str]=None,
-        file = typing.Optional[str]=None,
-        options = typing.Optional(list)=None,
-        in_bytes = typing.Optional[bytearray]=None,
+        op: typing.Optional[int] = None,
+        htype: typing.Optional[int] = None,
+        hlen: typing.Optional[int]=None,
+        hops: typing.Optional[int]=None,
+        xid: typing.Optional[int]=None,
+        secs: typing.Optional[int]=None,
+        flags: typing.Optional[bytearray]=None,
+        ciaddr: typing.Optional[typing.Union[str, bytearray]]=None,
+        yiaddr: typing.Optional[typing.Union[str, bytearray]]=None,
+        siaddr: typing.Optional[typing.Union[str, bytearray]]=None,
+        giaddr: typing.Optional[typing.Union[str, bytearray]]=None,
+        chaddr: typing.Optional[typing.Union[str, bytearray]]=None,
+        sname: typing.Optional[str]=None,
+        file: typing.Optional[str]=None,
+        options: typing.Optional[list]=None,
+        in_bytes: typing.Optional[bytearray]=None,
     ):
         self.headers = {}
         if in_bytes is not None:
@@ -97,34 +98,43 @@ class packet:
                 "file": file,
                 "options": []
             }
-            for option in options:
-                self.headers["options"].append({
-                    "code": option.getattr("code"),
-                    "len": len(option.getattr("byteval")),
-                    "byteval": option.getattr("byteval"),
-                    })
+            if options is not None:
+                for option in options:
+                    self.headers["options"].append({
+                        "code": option.getattr("code"),
+                        "len": len(option.getattr("byteval")),
+                        "byteval": option.getattr("byteval"),
+                        })
 
-        def to_bytes(self):
-            # See the diagram of the packet at the top as well as 
-            # the struct byte packing formats here:
-            # https://docs.python.org/3/library/struct.html
-            ret_bytes = struct.pack('!bbbb', self.op, self.htype, self.hlen, self.hops)
-            ret_bytes += struct.pack('!i', self.xid)
-            ret_bytes += struct.pack('!HH', self.secs, self.flags)
-            ret_bytes += struct.pack('!i', self.ciaddr)
-            ret_bytes += struct.pack('!i', self.yiaddr)
-            ret_bytes += struct.pack('!i', self.siaddr)
-            ret_bytes += struct.pack('!i', self.giaddr)
-            ret_bytes += struct.pack('!16s', self.chaddr)
-            ret_bytes += struct.pack('!64s', self.sname.encode('utf-8'))
-            ret_bytes += struct.pack('!128s', self.file.encode('utf-8'))
+    def to_bytes(self):
+        # See the diagram of the packet at the top as well as 
+        # the struct byte packing formats here:
+        # https://docs.python.org/3/library/struct.html
+        try:
+            ret_bytes = struct.pack('!bbbb', self.headers['op'], self.headers['htype'], self.headers['hlen'], self.headers['hops'])
+            ret_bytes += struct.pack('!i', self.headers['xid'])
+            ret_bytes += struct.pack('!HH', self.headers['secs'], self.headers['flags'])
+            ret_bytes += struct.pack('!i', self.headers['ciaddr'])
+            ret_bytes += struct.pack('!i', self.headers['yiaddr'])
+            ret_bytes += struct.pack('!i', self.headers['siaddr'])
+            ret_bytes += struct.pack('!i', self.headers['giaddr'])
+            ret_bytes += struct.pack('!16s', self.headers['chaddr'])
+            ret_bytes += struct.pack('!64s', self.headers['sname'].encode('utf-8'))
+            ret_bytes += struct.pack('!128s', self.headers['file'].encode('utf-8'))
 
             # magic cookie from the RFC
             ret_bytes += struct.pack('!BBBB', 99, 130, 83, 99) 
 
-            for option in self.options:
-                pass
-            return ret_bytes
+            for option in self.headers['options']:
+                ret_bytes += struct.pack('!B', option['code'])
+                ret_bytes += struct.pack(f'!{option["len"]}s', option['byteval'])
+
+            # Last option is the End Option
+            ret_bytes += struct.pack('!B', 255)
+        except struct.error as err:
+            print(f'Error encoding header values: {str(err)}')
+            return err
+        return ret_bytes
 
 class DHCPREQUEST(packet):
     pass
